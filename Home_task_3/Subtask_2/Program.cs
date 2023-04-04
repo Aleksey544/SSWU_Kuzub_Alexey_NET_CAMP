182b0dd6f6317f9687f819aeb553e0243ea153f3
Aleksey544 <hackercorporation544@gmail.com>
2023-04-04 23:51:12 +0300

    Home_task_3 Subtack_1 and Subtack_2 were done

---
+++ Home_task_3/Subtask_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Subtask_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string str, strToReplace;
+			string isContinue = "yes";
+
+			while (isContinue == "yes")
+			{
+				Console.Write("Введiть стрiчку: ");
+				str = Console.ReadLine();
+				Console.Write("Введiть стрiчку, на яку ми замiнемо пiдстрiчки з подвоєнням лiтер: ");
+				strToReplace = Console.ReadLine();
+
+				StringOperations stringOperations = new StringOperations(str);
+
+				Console.WriteLine("Iндекс другого входження заданої пiдстрiчки в текст: " +
+					stringOperations.IndexOfSecondOccurrence());
+				Console.WriteLine("Кiлькiсть слiв, що починаються з великої лiтри: " +
+					stringOperations.CapitalizedWordsAmount());
+				Console.WriteLine("Всi слова, що мiстили подвоєння лiтер, замiненi на заданий " +
+					"користувачем текст: " + 
+					stringOperations.ReplacingWordWithDoubledLetters(strToReplace));
+
+				Console.Write("Введiть yes, якщо хочете повторити виконання програми: ");
+				isContinue = Console.ReadLine();
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Home_task_3/Subtask_2/StringOperations.cs
@@ -0,0 +1,78 @@
+﻿namespace Subtask_2
+{
+	public class StringOperations
+	{
+		public string Str { get; private set; }
+		private string[] _subStrings;
+
+		public StringOperations(string str)
+		{
+			SetString(str);
+		}
+
+		public void SetString(string str)
+		{
+			Str = str;
+			_subStrings = Str.Split();
+		}
+
+		public int? IndexOfSecondOccurrence()
+		{
+			int? index;
+
+			if (_subStrings.Length > 1)
+			{
+				if (_subStrings[0] == _subStrings[1])
+				{
+					index = _subStrings[0].Length + 1;
+				}
+				else
+				{
+					index = null;
+				}
+			}
+			else
+			{
+				index = null;
+			}
+
+			return index;
+		}
+
+		public int CapitalizedWordsAmount()
+		{
+			int amount = 0;
+
+			for (int i = 0; i < _subStrings.Length; i++)
+			{
+				if (char.IsUpper(_subStrings[i][0]))
+				{
+					amount++;
+				}
+			}
+
+			return amount;
+		}
+
+		public string ReplacingWordWithDoubledLetters(string strToReplace)
+		{
+			string[] replacedSubStrings = _subStrings;
+			string replacedString = "";
+
+			for (int i = 0; i < replacedSubStrings.Length; i++)
+			{
+				for (int j = 1; j < replacedSubStrings[i].Length; j++)
+				{
+					if (_subStrings[i][j - 1] == _subStrings[i][j])
+					{
+						replacedSubStrings[i] = _subStrings[i].Replace(_subStrings[i], strToReplace);
+					}
+				}
+
+				replacedString += replacedSubStrings[i] + " ";
+			}
+
+			return replacedString;
+		}
+	}
+}
